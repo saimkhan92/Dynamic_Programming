42bb6e0ebe23b418991da9278dff9104d5166f70
saimkhan92 <saim.khan@colorado.edu>
2016-02-21 18:17:43 -0700

    new

---
+++ max_sub_matrix.py
@@ -0,0 +1,3 @@
+# Given a matrix of dimensions m x n having all entries as 1 or 0, find out the size of maximum size square sub-matrix with all 1s. 
+
+mat=[[1,0,0,1,1,1],[1,0,0,1,1,1],[1,0,0,1,1,1],[1,1,0,1,0,1],[0,0,0,1,0,1],[1,0,0,1,1,1],]
